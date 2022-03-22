81b9b820679a687b3113dc0c620c11cf292a0451
Carla Aleman <cdale134@learner.dvusd.org>
2022-03-22 13:07:37 -0700

    Circle change

--- DrawingApp/Circle.vb
+++ DrawingApp/Circle.vb
@@ -1,9 +1,9 @@
 ﻿Public Class Circle
     Public Property Pen As Pen
     Dim m_image As Image
-        Dim m_a As Point
-        Dim m_b As Point
-        Public Property w As Integer
+    Dim m_a As Point
+    Dim m_b As Point
+    Public Property w As Integer
         Public Property h As Integer
         Public Sub New(i As Image, a As Point, b As Point)
             Pen = Pens.Red
